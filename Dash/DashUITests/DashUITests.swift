b7bf00b30a866d56022afb8a2834256536d967ab
Johan K. Jensen <jj@johanjensen.dk>
2017-06-30 17:40:51 +0100

    Add a few tests

--- Dash/DashUITests/DashUITests.swift
+++ Dash/DashUITests/DashUITests.swift
@@ -4,9 +4,10 @@
 //
 
 import XCTest
+import VSMobileCenterExtensions
 
 class DashUITests: XCTestCase {
-        
+
     override func setUp() {
         super.setUp()
         
@@ -25,9 +26,65 @@ class DashUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAddDocset() {
+        let app = XCUIApplication()
+        
+        XCTAssertEqual(app.tables.cells.count, 0)
+        
+        MCLabel.labelStep("No docsets")
+        
+        app.navigationBars["Docsets"].buttons["Settings"].tap()
+        MCLabel.labelStep("Open settings to download docsets")
+        app.tables.cells.staticTexts["Cheat Sheets"].tap()
+        
+        let searchField = app.tables.searchFields["Find cheat sheets to download"]
+        searchField.tap()
+        searchField.typeText("NS")
+        
+        let emptyListTable = app.tables["Empty list"]
+        emptyListTable.cells.containing(.staticText, identifier: "NSDateFormatter").buttons["download button arrow"].tap()
+        MCLabel.labelStep("Downloading docset")
+        emptyListTable.buttons["Cancel"].tap()
+        
+        let backButton = app.navigationBars.buttons.element(boundBy: 0)
+        backButton.tap()
+        
+        let doneButton = app.navigationBars["Settings"].buttons["Done"]
+        doneButton.tap()
+        
+        sleep(2)
+        
+        XCTAssertEqual(app.tables.cells.count, 1)
+        MCLabel.labelStep("Docset downloaded")
     }
+
+
+    func testCategoriesCount() {
+        let tablesQuery = XCUIApplication().tables
+        tablesQuery.staticTexts["NSDateFormatter"].tap()
+        tablesQuery.staticTexts["Categories"].tap()
+        
+        XCTAssertEqual(tablesQuery.cells.count, 9)
+    }
+    
+    func testHideDocsets() {
+        let app = XCUIApplication()
+        let originalCount = app.tables.cells.count
+        XCTAssertGreaterThanOrEqual(originalCount, 1)
+        
+        MCLabel.labelStep("All downloaded docsets")
+
+        let navBar = app.navigationBars["Docsets"]
+        navBar.buttons["Edit"].tap()
+        MCLabel.labelStep("Edit docsets")
+        app.tables.staticTexts["NSDateFormatter"].tap()
+        MCLabel.labelStep("Toggle docsets")
+        navBar.buttons["Done"].tap()
+
+        let newCount = originalCount - 1
+        XCTAssertEqual(app.tables.cells.count, newCount)
+        MCLabel.labelStep("One docset hidden")
+    }
+
     
 }
